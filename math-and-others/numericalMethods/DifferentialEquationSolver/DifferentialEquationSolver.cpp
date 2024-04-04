a814f6831239647b9ec86d60e2a78c0f0932dc2e
yacshagames <ramondc@hotmail.com>
2024-04-03 23:46:12 -0500

    Update math-and-others\numericalMethods\DifferentialEquationSolver-rev5
    - The ModifiedEuler and Predictor_Corrector method is corrected
    - Order 4 Runge Kutta method added

--- math-and-others/numericalMethods/DifferentialEquationSolver/DifferentialEquationSolver.cpp
+++ math-and-others/numericalMethods/DifferentialEquationSolver/DifferentialEquationSolver.cpp
@@ -23,10 +23,10 @@ void Euler()
 	x = a; y = fa;
 	do {
 		y = y + h * f(x, y);
-		cout << "\n\tx = " << x;
-		cout << "\t\t\ty = " << y;
 		x = x + h;
-	} while (x <= b + h);
+		cout << "\n\tx = " << x;
+		cout << "\t\t\ty = " << y;		
+	} while (x <= b);
 	cgetch();
 }
 
@@ -39,7 +39,7 @@ double oper(double x, double y, double h)
 
 void ModifiedEuler()
 {
-	double fa, a, b, x, y, h;
+	double fa, a, b, x, y, h, k1, k2;
 	clrscr();
 	cout << "\n\tMETODO DE EULER MODIFICADO\n";
 	cout << "\n\tIngrese a = "; cin >> a;
@@ -48,7 +48,18 @@ void ModifiedEuler()
 	cout << "\tIngrese h = "; cin >> h;
 	x = a; y = fa;
 	do {
-		y = oper(x, y, h);
+		//y = oper(x, y, h);
+
+		//Predictor-corrector
+		//k1 = f(x, y);
+		//k2 = f(x + h, y + h * k1);
+		//y = y + 0.5*h*(k1 + k2);
+
+		// Euler modificado (Método del punto medio)
+		//y = (y + 0.5*h*f(x, y)) / (1 - f(x + h, y + h) / (y + h)*0.5*h);
+
+		y = y + h * f(x + 0.5*h, y + 0.5*h*f(x, y));
+
 		x = x + h;
 		cout << "\n\tx = " << x;
 		cout << "\t\ty = " << y;
@@ -56,7 +67,7 @@ void ModifiedEuler()
 	cgetch();
 }
 
-// METODO DE PREDICTOR CORRECTOR
+// METODO DE PREDICTOR CORRECTOR (Improved Euler (Heun's) Method Calculator)
 /*
 double f(double x, double y)
 {
@@ -65,9 +76,9 @@ double f(double x, double y)
 	
 }*/
 
-void Predictor_Corrector()
+void Predictor_Corrector( int iterMax )
 {
-	double fa, a, b, x, y, y1, h, yo, error = 1e-6;
+	double fa, a, b, x, y, y1, h, yo, error = 1e-6, k1;
 	int n = 1;
 	clrscr();
 	cout << "\n\tMETODO DE PREDICTOR CORRECTOR\n";
@@ -79,13 +90,18 @@ void Predictor_Corrector()
 	do {
 		//PREDICTOR: EULER
 		yo = y;
+		k1 = f(x, yo);
 		y = y + h * f(x, y);
 		//CORRECTOR: EULER MODIFICADO
+		int nn = 0;
 		do {
-			y1 = y;
-			y = yo + 0.5*h*(f(x, yo) + f(x + h, y));
-		} while (fabs(y - y1) < error);
 
+			y1 = y;
+			y = yo + 0.5*h*(k1 + f(x + h, y));
+
+		} while (	fabs(y - y1) > error && 
+					++nn < iterMax );
+		cout << "\t"<<nn;
 		cout << "\n\tx = " << x;
 		cout << "\t\t\ty = " << yo;
 		//		n=n+1;
@@ -100,19 +116,19 @@ void Predictor_Corrector()
 {
 	// Ecuacion Diferencial y'=y/x+2*x*y
 	return y / x + 2 * x*y;	
-}*/
-
+}
 
 double K1(double h, double y, double x)
 {
 	return h * f(x, y); // Ecuacion Diferencial
-			   // y'=-0.4y+0.2
+						// y'=-0.4y+0.2
 }
 double K2(double h, double y, double x, double k1)
 {
 	return h * f(x + h, y + k1);
-}
-void Runge_Kutta2ndOrder()
+}*/
+
+void Runge_Kutta2()
 {
 	double fa, a, b, x, y, h, k1, k2;
 	clrscr();
@@ -123,10 +139,13 @@ void Runge_Kutta2ndOrder()
 	cout << "\tIngrese h = ";    cin >> h;
 	x = a; y = fa;
 	do {
-		k1 = K1(h, y, x);
-		k2 = K2(h, y, x, k1);
+
+		k1 = f(x, y);
+		k2 = f(x + h, y + h * k1);
+
+		y = y + 0.5*h*(k1 + k2);
 		x = x + h;
-		y = y + (k1 + k2) / 2;
+
 		cout << "\n\tk1 = " << k1;
 		cout << "\tk2 = " << k2;
 		cout << "\tx = " << x;
@@ -135,12 +154,44 @@ void Runge_Kutta2ndOrder()
 	cgetch();
 }
 
+
+void Runge_Kutta4()
+{
+	double fa, a, b, x, y, h, k1, k2, k3, k4;
+	clrscr();
+	cout << "\n\tMETODO DE RUNGE KUTTA (ORDEN 4)\n";
+	cout << "\n\tIngrese a = ";  cin >> a;
+	cout << "\tIngrese b = ";    cin >> b;
+	cout << "\tIngrese f(a) = "; cin >> fa;
+	cout << "\tIngrese h = ";    cin >> h;
+	x = a; y = fa;
+	do {
+
+		k1 = f(x, y);
+		k2 = f(x + 0.5*h, y + 0.5*h*k1);
+		k3 = f(x + 0.5*h, y + 0.5*h*k2);
+		k4 = f(x + h, y + h*k3);
+
+		y = y + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+		x = x + h;
+
+		cout << "\n\tk1 = " << k1;
+		cout << "\tk2 = " << k2;
+		cout << "\tk3 = " << k3;
+		cout << "\tk4 = " << k4;
+		cout << "\tx = " << x;
+		cout << "\ty = " << y;
+	} while (x < b);
+	cgetch();
+}
+
 int main() {
 	
-	Euler();
-	ModifiedEuler();
-	Predictor_Corrector();
-	Runge_Kutta2ndOrder();
+	//Euler();
+	//ModifiedEuler();
+	Predictor_Corrector(50);
+	Runge_Kutta2();
+	Runge_Kutta4();
 
 	return 1;
 }
