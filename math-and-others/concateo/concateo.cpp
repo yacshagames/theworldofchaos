518a7fc7e2888cf1986d76647852ac5de1c4ae7e
yacshagames <ramondc@hotmail.com>
2024-04-19 13:05:39 -0500

    Update math-and-others\concateo - Porting to VC++ 2017 using winbgi - rev2
    - Update math-and-others\concateo - Porting to VC++ 2017 using winbgi
    - Using strcpy_s and strcat_s instead old and not secure strcpy and strcat
    - Update credits

--- math-and-others/concateo/concateo.cpp
+++ math-and-others/concateo/concateo.cpp
@@ -1,8 +1,14 @@
 /***********************************************************************
- :::CONCATENEO DE CADENAS:::
-  >> Versión 1.0 26-XI-2000
+ ::: CONCATENEO DE CADENAS :::
+
  Clase Cstring que soporta concateneación de cadenas, es decir,
  que junta los contenidos de 2 cadenas y los añade a otra cadena destino.
+
+ Esta librería la desarrollé por los años 2000, y era útil en compiladores
+ viejos que no soportaban la STL, pero ahora con la inclusión de la clase
+ estándar std::string, esta librería a quedado obsoleta, pero la estoy
+ dejando a disposición de la comunidad, como un ejemplo didáctico de la
+ sobrecarga del operador +, en objetos de una clase
 
  Ejemplo:
 
@@ -10,17 +16,32 @@
  destino = primera + segunda; //concateneo
  destino.Escribir(); //escribe el destino en pantalla
 
- :::Programado por:::
- José Luis De La Cruz Lázaro
- YACSHA - Software & Desing 2002
-  >> yacsha@elmundodelcaos.tk
-  >> www.elmundodelcaos.tk -> EL MUNDO DEL CAOS - Unlimited Programming
-  >> www.foros3d.tk  -> Foros de discusión  Flash/ActionScript - C/C++
+Programmed by:
+
+	JOSE LUIS DE LA CRUZ LAZARO
+	ramondc@hotmail.com
+
+	UNIVERSIDAD NACIONAL DE INGENIERIA
+	Faculty of Electrical and Electronic Engineering
+	Lima-Peru
+
+	YACSHA - Software & Desing
+	>> The World of chaos - EL MUNDO DEL CAOS - Unlimited Programming
+
+ HISTORY...
+
+ >> Version 2 - 19-IV-2024
+	- Update math-and-others\concateo - Porting to VC++ 2017 using winbgi
+	- Using strcpy_s and strcat_s instead old and not secure strcpy and strcat
+
+>> Version 1 - 26-XI-2000
+	- First version for Borland C++ 3.1 and Turbo C 3.0
 
 ***********************************************************************/
 
-#include <string.h>
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 class CString
 {
@@ -28,50 +49,54 @@ public:
 	char *cadena;
 
 	CString() { cadena = NULL; } //constructor por defecto
-	CString(char *cadini); // inicia el string con una cadena de caracteres
+	CString(const char *cadini); // inicia el string con una cadena de caracteres
 	//~CString();              //destructor "finta"
 
    //operador de concateneo, entre un objeto de esta
    //clase ( cadena izquierda ) y str ( cadena derecha )
    //y se retorna un objeto destino = CadIzq + CadDer
-	CString operator +(CString str);
+	CString operator +(const CString&  str);
 
 	void Escribir();  //escribe la cadena en pantalla
 };
 
-CString::CString(char *cadini)
+CString::CString(const  char *cadini)
 {
 	//asigna espacio para cadena
-	cadena = new char[strlen(cadini) + 1];
-	strcpy(cadena, cadini); // cadena = cadini
+	unsigned int tamanoCadena = static_cast<unsigned int>(strlen(cadini) + 1);
+	cadena = new char[tamanoCadena];
+	strcpy_s(cadena, tamanoCadena, cadini); // cadena = cadini
 }
 
-CString CString::operator +(CString str)
+CString CString::operator +(const CString& str)
 {
 	CString destino;
 	//se asigna el nuevo espacio para destino
-	destino.cadena = new char[strlen(cadena) + strlen(str.cadena) + 1];
-	strcpy(destino.cadena, cadena); //destino = CadIzq
-	strcat(destino.cadena, str.cadena);    //destino = primera + CadDer
+	unsigned int tamanoCadena = static_cast<unsigned int>(strlen(cadena) + strlen(str.cadena) + 1);
+	destino.cadena = new char[tamanoCadena];
+	strcpy_s(destino.cadena, tamanoCadena, cadena); //destino = CadIzq
+	strcat_s(destino.cadena, tamanoCadena, str.cadena);    //destino = primera + CadDer
 	return destino;
 }
 
 void CString::Escribir()
 {
-	printf("%s", cadena);
+	cout << cadena;
 }
 
 //un ejemplo
-void main()
+int main()
 {
 	CString primera("¨Una suma de cadenas?"),
-		segunda("­Imposible!"),
-		destino;
+			segunda("­Imposible!"),
+			destino;
 
 	destino = primera + segunda; //concateneo
 	// destino = primera.operator+( segunda );
 
 	destino.Escribir(); //escribe el destino en pantalla
-	printf("\n");
+	
+	cout << endl;
 
+	return 1;
 }
