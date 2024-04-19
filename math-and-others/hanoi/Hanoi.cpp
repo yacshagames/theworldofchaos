16de02ccc8c9383c5e5ed2d7142db6a7e3c52523
yacshagames <ramondc@hotmail.com>
2024-04-19 16:57:13 -0500

    Update math-and-others\hanoi - Porting to VC++ 2017
    Update to C++17

--- math-and-others/hanoi/Hanoi.cpp
+++ math-and-others/hanoi/Hanoi.cpp
@@ -1,19 +1,34 @@
 /***********************************************************************
  ::: LAS TORRES DE HANOI :::
-  >> Versión 1.0 19-X-2000
+ 
  Clásico algoritmo de las torres de hanoi solucionado mediante una
  función recursiva.
 
- :::Programado por:::
- José Luis De La Cruz Lázaro
- YACSHA - Software & Desing 2002
-  >> yacsha@elmundodelcaos.tk
-  >> www.elmundodelcaos.tk -> EL MUNDO DEL CAOS - Unlimited Programming
-  >> www.foros3d.tk  -> Foros de discusión  Flash/ActionScript - C/C++
+Programmed by:
+
+	JOSE LUIS DE LA CRUZ LAZARO
+	ramondc@hotmail.com
+
+	UNIVERSIDAD NACIONAL DE INGENIERIA
+	Faculty of Electrical and Electronic Engineering
+	Lima-Peru
+
+	YACSHA - Software & Desing
+	>> The World of chaos - EL MUNDO DEL CAOS - Unlimited Programming
+
+ HISTORY...
+
+ >> Version 2 - 19-IV-2024
+	- Update math-and-others\hanoi - Porting to VC++ 2017
+
+>> Version 1 - 19-X-2000
+	- First version for Borland C++ 3.1 and Turbo C 3.0
 
 ***********************************************************************/
 
-#include <iostream.h>
+#include <iostream>
+
+using namespace std;
 
 void Hanoi(int n, char A, char B, char C)
 {
@@ -25,10 +40,12 @@ void Hanoi(int n, char A, char B, char C)
 	}
 }
 
-void main()
+int main()
 {
 	int n;
 	cout << "\n\nIngrese el n£mero de discos por torre: ";
 	cin >> n;
 	Hanoi(n, 'A', 'B', 'C');
+
+	return 1;
 }
