6e6c449d297f8e319bb46cd118c9a94c1038ead0
yacshagames <ramondc@hotmail.com>
2024-04-19 12:35:29 -0500

    Update math-and-others\aratorom - Porting to VC++ 2017 using winbgi

--- math-and-others/aratorom/aratorom.cpp
+++ math-and-others/aratorom/aratorom.cpp
@@ -1,26 +1,44 @@
 /***********************************************************************
  :::CONVERTIDOR DE ARABIGOS A ROMANOS:::
-  >> Versión 1.0 31-X-2000
+
  Convierte un número Arabigo (numeración ordinaria) de 4 cifras
  a números Romanos.
 
  Ejemplo:
  Si ingreso 59 y retorna LIX.
 
- :::Programado por:::
- José Luis De La Cruz Lázaro
- YACSHA - Software & Desing 2002
-  >> yacsha@elmundodelcaos.tk
-  >> www.elmundodelcaos.tk -> EL MUNDO DEL CAOS - Unlimited Programming
-  >> www.foros3d.tk  -> Foros de discusión  Flash/ActionScript - C/C++
+Programmed by:
+
+	JOSE LUIS DE LA CRUZ LAZARO
+	ramondc@hotmail.com
+
+	UNIVERSIDAD NACIONAL DE INGENIERIA
+	Faculty of Electrical and Electronic Engineering
+	Lima-Peru
+
+	YACSHA - Software & Desing
+	>> The World of chaos - EL MUNDO DEL CAOS - Unlimited Programming
+
+ HISTORY...
+
+ >> Version 2 - 19-IV-2024
+	- Update math-and-others\aratorom - Porting to VC++ 2017 using winbgi
+
+ >> Version 1 - 31-X-2000
+	- First version for Borland C++ 3.1 and Turbo C 3.0
 
 ***********************************************************************/
 
-#include <iostream.h>
+#include <iostream>
+
+using namespace std;
 
 void transf(int cifra, int orden) {
+
 	char  ui, um, uf;
+
 	switch (orden) {
+
 	case 0: ui = 'I';
 		um = 'V';
 		uf = 'X';
@@ -37,6 +55,7 @@ void transf(int cifra, int orden) {
 		break;
 	}
 	switch (cifra) {
+
 	case 1: cout << ui; break;
 	case 2: cout << ui << ui;  break;
 	case 3: cout << ui << ui << ui; break;
@@ -48,9 +67,13 @@ void transf(int cifra, int orden) {
 	case 9: cout << ui << uf; break;
 	}
 }
-void main() {
+
+int main() {
+
 	int N, a, b, c, d;
+
 	cout << "Ingrese un numero arabigo: "; cin >> N;
+
 	d = N % 10;
 	c = N % 100 / 10;
 	b = N % 1000 / 100;
@@ -60,5 +83,6 @@ void main() {
 	transf(b, 2);
 	transf(c, 1);
 	transf(d, 0);
-
+	
+	return 1;
 }
