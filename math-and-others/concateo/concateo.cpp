65db335f760425dd5e2c4790f7d2ffa721adf086
yacshagames <ramondc@hotmail.com>
2024-04-19 12:43:00 -0500

    Update math-and-others\concateo - Porting to VC++ 2017 using winbgi
    - Update code format

--- math-and-others/concateo/concateo.cpp
+++ math-and-others/concateo/concateo.cpp
@@ -13,7 +13,7 @@
  :::Programado por:::
  José Luis De La Cruz Lázaro
  YACSHA - Software & Desing 2002
-  >> yacsha@elmundodelcaos.tk    
+  >> yacsha@elmundodelcaos.tk
   >> www.elmundodelcaos.tk -> EL MUNDO DEL CAOS - Unlimited Programming
   >> www.foros3d.tk  -> Foros de discusión  Flash/ActionScript - C/C++
 
@@ -24,54 +24,54 @@
 
 class CString
 {
-  public:
-  char *cadena;
+public:
+	char *cadena;
 
-  CString(){ cadena =NULL; } //constructor por defecto
-  CString( char *cadini ); // inicia el string con una cadena de caracteres
-  //~CString();              //destructor "finta"
+	CString() { cadena = NULL; } //constructor por defecto
+	CString(char *cadini); // inicia el string con una cadena de caracteres
+	//~CString();              //destructor "finta"
 
- //operador de concateneo, entre un objeto de esta
- //clase ( cadena izquierda ) y str ( cadena derecha )
- //y se retorna un objeto destino = CadIzq + CadDer
-  CString operator +( CString str );
+   //operador de concateneo, entre un objeto de esta
+   //clase ( cadena izquierda ) y str ( cadena derecha )
+   //y se retorna un objeto destino = CadIzq + CadDer
+	CString operator +(CString str);
 
-  void Escribir();  //escribe la cadena en pantalla
+	void Escribir();  //escribe la cadena en pantalla
 };
 
-CString::CString( char *cadini )
+CString::CString(char *cadini)
 {
- //asigna espacio para cadena
- cadena = new char[ strlen(cadini)+1 ];
- strcpy(cadena,cadini); // cadena = cadini
+	//asigna espacio para cadena
+	cadena = new char[strlen(cadini) + 1];
+	strcpy(cadena, cadini); // cadena = cadini
 }
 
-CString CString::operator +( CString str )
+CString CString::operator +(CString str)
 {
- CString destino;
- //se asigna el nuevo espacio para destino
- destino.cadena = new char[strlen(cadena)+strlen(str.cadena)+1];
- strcpy(destino.cadena,cadena); //destino = CadIzq
- strcat(destino.cadena,str.cadena);    //destino = primera + CadDer
- return destino;
+	CString destino;
+	//se asigna el nuevo espacio para destino
+	destino.cadena = new char[strlen(cadena) + strlen(str.cadena) + 1];
+	strcpy(destino.cadena, cadena); //destino = CadIzq
+	strcat(destino.cadena, str.cadena);    //destino = primera + CadDer
+	return destino;
 }
 
 void CString::Escribir()
 {
- printf( "%s",cadena );
+	printf("%s", cadena);
 }
 
 //un ejemplo
 void main()
 {
- CString primera("¨Una suma de cadenas?"),
-	 segunda("­Imposible!"),
-	 destino;
+	CString primera("¨Una suma de cadenas?"),
+		segunda("­Imposible!"),
+		destino;
 
- destino = primera + segunda; //concateneo
- // destino = primera.operator+( segunda );
+	destino = primera + segunda; //concateneo
+	// destino = primera.operator+( segunda );
 
- destino.Escribir(); //escribe el destino en pantalla
- printf("\n");
+	destino.Escribir(); //escribe el destino en pantalla
+	printf("\n");
 
 }
