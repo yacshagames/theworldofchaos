fb826783a9bebea0e6042004d539f2ec272bdf45
yacshagames <ramondc@hotmail.com>
2024-04-22 20:00:10 -0500

    Update math-and-others\mcdnum- Porting to VC++ 2017
    Update code format

--- math-and-others/mcdnum/mcdnum.cpp
+++ math-and-others/mcdnum/mcdnum.cpp
@@ -1,49 +1,51 @@
 #include"fstream.h"
 #include"conio.h"
 
-int a[30],n;
+int a[30], n;
 
 int MCD()
 {
 
-int S=1,i,j,salir;
+	int S = 1, i, j, salir;
 
-for(i=0;i<n;i++)
-if(a[i]==1){salir=1;break;}else salir=0;
+	for (i = 0; i < n; i++)
+		if (a[i] == 1) { salir = 1; break; }
+		else salir = 0;
 
-i=2;
+	i = 2;
 
-if(!salir){
+	if (!salir) {
 
-while(S!=0){
-S=0;
-for(j=0;j<n;j++)
-S+=a[j]%i;
-i++;
-}
-i--;
+		while (S != 0) {
+			S = 0;
+			for (j = 0; j < n; j++)
+				S += a[j] % i;
+			i++;
+		}
+		i--;
 
-if(!S){
-for(j=0;j<n;j++)
-a[j]/=i;
-return i*MCD();
-}
+		if (!S) {
+			for (j = 0; j < n; j++)
+				a[j] /= i;
+			return i * MCD();
+		}
 
-}else return 1;
+	}
+	else return 1;
 
 }
 
 void main()
 {
-clrscr();
-int i;
-cout<<"Ingrese n:";cin>>n;
+	clrscr();
+	int i;
+	cout << "Ingrese n:"; cin >> n;
 
-cout<<"Ingrese los numeros: ";
+	cout << "Ingrese los numeros: ";
 
-for(i=0;i<n;i++)
-cin>>a[i];
+	for (i = 0; i < n; i++)
+		cin >> a[i];
 
-cout<<"El MCD es :"<<MCD();
-getch();
+	cout << "El MCD es :" << MCD();
+	getch();
 }
