ef8c8ad2d2b224a0db65132dfa6c70e6f6c1e0fd
yacshagames <ramondc@hotmail.com>
2024-04-23 00:16:06 -0500

    Update math-and-others\mcdnum - Updated to C++17
    - Add credits and history
    - Translate program from Spanish to English

--- math-and-others/mcdnum/mcdnum.cpp
+++ math-and-others/mcdnum/mcdnum.cpp
@@ -1,51 +1,100 @@
-#include"fstream.h"
-#include"conio.h"
+/***********************************************************************
+ ::: GREATEST COMMON DIVISOR :::
 
-int a[30], n;
+Function to solve the greatest common divisor of a group of numbers
 
-int MCD()
+Programmed by:
+
+	JOSE LUIS DE LA CRUZ LAZARO
+	ramondc@hotmail.com
+
+	UNIVERSIDAD NACIONAL DE INGENIERIA
+	Faculty of Electrical and Electronic Engineering
+	Lima-Peru
+
+	YACSHA - Software & Desing
+	>> The World of chaos - EL MUNDO DEL CAOS - Unlimited Programming
+
+ HISTORY...
+
+ >> Version 2 - 23-IV-2024
+	- Update math-and-others\mcdnum - Porting to VC++ 2017
+
+>> Version 1 - 19-IV-2000
+	- First version for Borland C++ 3.1 and Turbo C 3.0
+
+***********************************************************************/
+
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::cin;
+
+int GCD(std::vector<int>& a )
 {
 
-	int S = 1, i, j, salir;
+	int S = 1, i, j, salir, n = static_cast<int>(a.size());
 
 	for (i = 0; i < n; i++)
-		if (a[i] == 1) { salir = 1; break; }
-		else salir = 0;
+		if (a[i] == 1){
+			salir = 1;
+			break;
+		}else
+			salir = 0;
 
 	i = 2;
 
+	bool ThereAreNoMoreDividers = true;
+
 	if (!salir) {
 
-		while (S != 0) {
+		while (S != 0 && ThereAreNoMoreDividers) {
 			S = 0;
-			for (j = 0; j < n; j++)
+			ThereAreNoMoreDividers = true;
+			for (j = 0; j < n; j++) {
+				ThereAreNoMoreDividers &= (i <= a[j]);
 				S += a[j] % i;
+			}
+
 			i++;
 		}
 		i--;
 
-		if (!S) {
+		if ( S==0 ) {
 			for (j = 0; j < n; j++)
 				a[j] /= i;
-			return i * MCD();
+			return i * GCD(a);
 		}
 
-	}
-	else return 1;
-
+	}	
+	
+	return 1;
 }
 
-void main()
+int main()
 {
-	clrscr();
-	int i;
-	cout << "Ingrese n:"; cin >> n;
+	int i, n; 
+	
+	std::vector<int> a;
 
-	cout << "Ingrese los numeros: ";
+	cout << "GREATEST COMMON DIVISOR:" << std::endl << std::endl;
 
-	for (i = 0; i < n; i++)
+	cout << "Enter the number of numbers to calculate: ";
+
+	cin >> n;
+
+	a.resize(n, 0);
+
+	cout << std::endl << "Enter the numbers:" << std::endl << std::endl;
+
+	for (i = 0; i < n; i++) {
+		cout << "Number " << i + 1 << " : ";
 		cin >> a[i];
+	}
 
-	cout << "El MCD es :" << MCD();
-	getch();
+	cout << std::endl << "The GCD is : " << GCD(a) << std::endl;
+	
+
+	return 1;
 }
