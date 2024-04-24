65bf9b6be00bb2a9de1b3b8230f7edc31da770ce
yacshagames <ramondc@hotmail.com>
2024-04-24 01:21:26 -0500

    Update math-and-others\bouncingball - Porting to VC++ 2017 - Update to C++17
    - The code is updated to the new C++ 17 standard
    - Add conio.h library compatible with VC++17 (WinBGI)

--- math-and-others/bouncingball/bouncingball.cpp
+++ math-and-others/bouncingball/bouncingball.cpp
@@ -8,86 +8,97 @@
  :::Programado por:::
  José Luis De La Cruz Lázaro
  YACSHA - Software & Desing 2002
-  >> yacsha@elmundodelcaos.tk    
+  >> yacsha@elmundodelcaos.tk
   >> www.elmundodelcaos.tk -> EL MUNDO DEL CAOS - Unlimited Programming
   >> www.foros3d.tk  -> Foros de discusión  Flash/ActionScript - C/C++
 
 ***********************************************************************/
 
-#include <iostream.h>
-#include <conio.h>
-#include <dos.h>
-#include <stdlib.h>
+#include <iostream>
+#include "conio.h" // gotoxy
+#include <conio.h> // _kbhit
+
+using std::cout;
+using std::cin;
 
 class Pelota
 {
- public:
+public:
 
- int X,Y;
- int dx,dy;
+	int X, Y;
+	int dx, dy;
 
- Pelota();
- Pelota(int x, int y);
+	Pelota();
+	Pelota(int x, int y);
 
- void Mover();
+	void Mover();
 
 };
 
 Pelota::Pelota()
 {
- X=40;
- Y=12;
- dx=1;
- dy=1;
+	X = 40;
+	Y = 12;
+	dx = 1;
+	dy = 1;
 }
 
-Pelota::Pelota( int x, int y)
+Pelota::Pelota(int x, int y)
 {
- X=x;
- Y=y;
- dx=1;
- dy=1;
+	X = x;
+	Y = y;
+	dx = 1;
+	dy = 1;
 }
 
 void Pelota::Mover()
 {
- if( X==80 ) dx=-1;
- if( X==1 ) dx=1;
+	if (X == 80) 
+		dx = -1;
 
- if( Y==25 ) dy=-1;
- if( Y==1 ) dy=1;
+	if (X == 1)
+		dx = 1;
 
- gotoxy(X,Y);
- cout<<' ';
+	if (Y == 25)
+		dy = -1;
 
- X+=dx;
- Y+=dy;
+	if (Y == 1)
+		dy = 1;
 
- gotoxy(X,Y);
- cout<<'O';
+	gotoxy(X, Y);
+	cout << ' ';
+
+	X += dx;
+	Y += dy;
+
+	gotoxy(X, Y);
+	cout << 'O';
 
 }
 
-void main()
+int main()
 {
- Pelota bola[8];
- int i;
- clrscr();
+	Pelota bola[8];
+	int i;
+	clrscr();
 
- randomize();
+	randomize();
 
- for( i=0;i<8;i++)
- {
-  bola[i].X = 1+random(80);
-  bola[i].Y = 1+random(25);
- }
+	for (i = 0; i < 8; i++)
+	{
+		bola[i].X = 1 + random(80);
+		bola[i].Y = 1 + random(25);
+	}
 
- while( !kbhit() )
- {
-  delay( 100 );
-  //clrscr();
-  for( i=0;i<8;i++)
-   bola[i].Mover();
- }
+	while (_kbhit()==false)
+	{
+		//delay(100);
+		Sleep(100);
+
+		for (i = 0; i < 8; i++)
+			bola[i].Mover();
+	}
+
+	return 1;
 
 }
