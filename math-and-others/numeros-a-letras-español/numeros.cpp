81a49b95f820dd30c0afef3b1821ae561a09448d
yacshagames <ramondc@hotmail.com>
2024-04-23 23:01:55 -0500

    Update math-and-others\numeros-a-letras-español - Update to C++17
    - The code is updated to the new C++ 17 standard
    - The credits and version history are updated
    - Function parameters are ordered to constants, to indicate that the variables passed as reference parameters will not be modified
    - Accents are removed so that the text appears correct in the console window

--- math-and-others/numeros-a-letras-español/numeros.cpp
+++ math-and-others/numeros-a-letras-español/numeros.cpp
@@ -1,41 +1,64 @@
 /*********************************************************************
-LECTURA DE NUMEROS
+:: LECTURA DE NUMEROS - Español ::
+
 Este programa lee un numero entero y retorna su equivalente en letras.
 Ejm:
 Si ingreso 3465
 el programa retorna "tres mil cuatrocientos sesenta y cinco"
 
-Version 1.1 - 9 Feb 2000
-- Se supero el error de que cuando ingresabas un numero que terminaba
-  en tres ceros, se escribia "cero" al final de la cadena.
+ Developed by:
 
-Version 1.0 - 2 Dic 1999
-- Lectura de numeros menores que 100000.
+	JOSE LUIS DE LA CRUZ LAZARO
+	ramondc@hotmail.com
 
-programado por:
-JOSE LUIS DE LA CRUZ LAZARO
-ramondc@hotmail.com
-Pagina Web: http://www.geocities.com/joseluisdl/jldl.htm
+	UNIVERSIDAD NACIONAL DE INGENIERIA
+	Faculty of Electrical and Electronic Engineering
+	Lima-Peru
+
+	YACSHA - Software & Desing
+	>> The World of chaos - EL MUNDO DEL CAOS - Unlimited Programming
+
+ HISTORY...
+
+  >> Version 2 - 23-IV-2024
+	- The code is updated to the new C++ 17 standard
+	- The credits and version history are updated
+	- Function parameters are ordered to constants, to indicate that
+	  the variables passed as reference parameters will not be modified
+	- Accents are removed so that the text appears correct in the
+	  console window
+
+  >> Version 1.1 - 09-II-2000
+	- Se supero el error de que cuando ingresabas un numero que terminaba
+	  en tres ceros, se escribia "cero" al final de la cadena.
+
+  >> Version 1.0 - 02-XII-1999
+	- Lectura de numeros menores que 100000.
+	
 **********************************************************************/
 
-#include <iostream.h>
-#include <conio.h>
+#include <iostream>
+#include <vector>
+#include <string>
 
-char *Unidad[10] = { "cero", "uno", "dos","tres","cuatro","cinco", "seis",
-		   "siete", "ocho", "nueve" };
+using std::cout;
+using std::cin;
 
-char *Diez[10] = { "diez", "once", "doce", "trece", "catorce", "quince",
-		 "dieciseis", "diecisiete", "dieciocho", "diecinueve" };
+std::vector<std::string> Unidad = {	"cero", "uno", "dos","tres","cuatro","cinco", "seis",
+									"siete", "ocho", "nueve" };
 
-char *Decena[10] = { "cero","diez","veinte","treinta","cuarenta","cincuenta",
-		   "sesenta","setenta","ochenta","noventa" };
+std::vector<std::string> Diez = {	"diez", "once", "doce", "trece", "catorce", "quince",
+									"dieciseis", "diecisiete", "dieciocho", "diecinueve" };
 
-char *Centena[10] = { "cien", "ciento", "doscientos", "trescientos","cuatrocientos",
-			 "quinientos", "seiscientos", "setecientos",
-			 "ochocientos", "novecientos" };
-char Millar[] = "mil";
+std::vector<std::string> Decena = { "cero","diez","veinte","treinta","cuarenta","cincuenta",
+									"sesenta","setenta","ochenta","noventa" };
 
-void Nombrar_Centena(int Num)
+std::vector<std::string> Centena = {	"cien", "ciento", "doscientos", "trescientos","cuatrocientos",
+										"quinientos", "seiscientos", "setecientos",
+										"ochocientos", "novecientos" };
+std::string Millar = "mil";
+
+void Nombrar_Centena(const unsigned int& Num)
 {
 	//Num es una centena de la forma abc
 	//descomponemos num en: a, b y c
@@ -47,7 +70,8 @@ void Nombrar_Centena(int Num)
 	if (a > 0)
 		if (b == 0 && c == 0)       //si el Num = 100
 			cout << Centena[0] << " ";
-		else cout << Centena[a] << " "; ///si 100<Num<200
+		else
+			cout << Centena[a] << " "; ///si 100<Num<200
 
 
 	if (b > 0)
@@ -58,39 +82,49 @@ void Nombrar_Centena(int Num)
 
 	if (c > 0)                 //imprime las unidades
 	{
-		if (b > 1) cout << "y ";
-		if (b != 1) cout << Unidad[c] << " ";
+		if (b > 1)
+			cout << "y ";
+		
+		if (b != 1)
+			cout << Unidad[c] << " ";
 	}
 
 }
 
-void Nombrar_Numero(long Num)
+void Nombrar_Numero(const unsigned int& Num)
 {
 	//Num es un numero menor que 100000
-	if (Num == 0)cout << Unidad[0]; //si Num=0
+	if (Num == 0)
+		cout << Unidad[0]; //si Num=0
+	
 	if (Num < 100000) //si Num 1<
 	{
 		int Num_mil = (Num - Num % 1000) / 1000;
-		if (Num_mil > 1) Nombrar_Centena(Num_mil);
-		if (Num >= 1000) cout << Millar << " ";
+		
+		if (Num_mil > 1)
+			Nombrar_Centena(Num_mil);
+		
+		if (Num >= 1000) 
+			cout << Millar << " ";
+		
 		Nombrar_Centena(Num % 1000);
-	}
-	else cout << "Lo siento, el n£mero ingresado es mayor o igual a 100000";
+
+	}else 
+		cout << "Lo siento, el numero ingresado es mayor o igual a 100000";
 
 }
 
-
-void main()
+int main()
 {
-
-	clrscr();
-	long Num;
-	cout << "Ingrese un n£mero menor que 100000: ";
+	unsigned int Num;
+	cout << "Ingrese un numero menor que 100000: ";
 	cin >> Num;
 
-	cout << "\nEl n£mero en letras ingresado es:\n\n";
+	cout << "\nEl numero en letras ingresado es:\n\n";
 	Nombrar_Numero(Num);
 
-	getch();
+	cout << std::endl;
+
+	return 1;
 
 }
