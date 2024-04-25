21f02b4d1e8ae76c884bae8d0a0d8157af20d714
yacshagames <ramondc@hotmail.com>
2024-04-25 16:00:01 -0500

    Update math-and-others\cmatrix - Update code format

--- math-and-others/cmatrix/cmatrix.h
+++ math-and-others/cmatrix/cmatrix.h
@@ -43,24 +43,24 @@ Pagina Web ( EL MUNDO DEL CAOS ):
 //esto puede deberse a que no hay memoria suficiente para crear las matrices
 //sugiero que cambie el valor de _MAX a un valor menor al que contiene
 //actualmente
-const _MAX=6; //Maximo numero de elementos con los que se va a trabajar
+const _MAX = 6; //Maximo numero de elementos con los que se va a trabajar
 
 class CMatriz
 {
-//    protected:
-     private:
-    //VARIABLES MIEMBRO
-    int FIL;
-    int COL;
-    int ORDEN;
+	//    protected:
+private:
+	//VARIABLES MIEMBRO
+	int FIL;
+	int COL;
+	int ORDEN;
 
-    public:
+public:
 	double Elemento[_MAX][_MAX]; //Contiene los elementos de la matriz
 	//CONSTRUCTORES
 	CMatriz(); //constuye una matriz cuadrada de orden _MAX
 	CMatriz(int filas, int columnas); //constructor para matrices no cuadradas
 	CMatriz(int orden); //constructor para matrices cuadradas
-	~CMatriz(){ }; //destructor
+	~CMatriz() { }; //destructor
 
 	//FUNCIONES
 	void Inicializar(double elemento); //Copia a todos los elementos
@@ -71,16 +71,16 @@ class CMatriz
 				 //desde el teclado
 	void Escribir(); //Escribe la matriz en pantalla
 
-	int Fil(){ return FIL; }  //retorna el numero de filas de la matriz
-	int Col(){ return COL; }  //retorna el numero de columnas de la matriz
-	int Orden(){ return ORDEN; }//retorna el orden de la matriz en caso
-				    //de ser una matriz cuadrada y en caso
-				    //contrario retorna -1
+	int Fil() { return FIL; }  //retorna el numero de filas de la matriz
+	int Col() { return COL; }  //retorna el numero de columnas de la matriz
+	int Orden() { return ORDEN; }//retorna el orden de la matriz en caso
+					//de ser una matriz cuadrada y en caso
+					//contrario retorna -1
 
 	//DEFINE LAS OPERACIONES ENTRE MATRICES
 	friend CMatriz operator-(CMatriz &A); 		 //operacion -A  NEGATIVO DE UNA MATRIZ
-	friend CMatriz operator+(CMatriz &A,CMatriz &B); //operaci¢n A+B SUMA DE MATRICES
-	friend CMatriz operator-(CMatriz &A,CMatriz &B); //operaci¢n A-B RESTA DE MATRICES
+	friend CMatriz operator+(CMatriz &A, CMatriz &B); //operaci¢n A+B SUMA DE MATRICES
+	friend CMatriz operator-(CMatriz &A, CMatriz &B); //operaci¢n A-B RESTA DE MATRICES
 	friend CMatriz operator*(double k, CMatriz &A);  //operaci¢n k*B (k es una constante)
 	friend CMatriz operator*(CMatriz &A, CMatriz &B);//operaci¢n A*B (Producto matricial)
 };
@@ -89,36 +89,36 @@ class CMatriz
 //CONSTRUYE LA MATRIZ CON VALORES POR DEFECTO: FIL=COL=10;
 CMatriz::CMatriz(void)
 {
-   FIL = COL = ORDEN = _MAX;//La matriz es cuadrada
-   Inicializar(0);
+	FIL = COL = ORDEN = _MAX;//La matriz es cuadrada
+	Inicializar(0);
 }
 //////////////////////////////////////////////////////////////////////
 //CONSTRUYE UNA MATRIZ CUADRADA DE ORDEN "orden"
 CMatriz::CMatriz(int orden)
 {
-   FIL = COL = ORDEN = orden;//La matriz es cuadrada
-   Inicializar(0);
+	FIL = COL = ORDEN = orden;//La matriz es cuadrada
+	Inicializar(0);
 }
 //////////////////////////////////////////////////////////////////////
 //CONSTRUYE UNA MATRIZ ESPECIFICANDO EL NUMERO DE FILAS Y COLUMNAS
-CMatriz::CMatriz(int filas, int columnas )
+CMatriz::CMatriz(int filas, int columnas)
 {
-  FIL=filas;
-  COL=columnas;
+	FIL = filas;
+	COL = columnas;
 
-   if(FIL==COL)ORDEN=FIL;//La matriz es cuadrada
-     else ORDEN=-1; //para detectar cuando una matriz no es cuadrada: Orden =-1
+	if (FIL == COL)ORDEN = FIL;//La matriz es cuadrada
+	else ORDEN = -1; //para detectar cuando una matriz no es cuadrada: Orden =-1
 
-   Inicializar(0);
+	Inicializar(0);
 }
 //////////////////////////////////////////////////////////////////////
 //INICIALIZAR LOS ELEMENTOS DE LA MATRIZ
 void CMatriz::Inicializar(double elemento)
 {
 
-  for(int i=0;i<FIL;i++)
-	for(int j=0;j<COL;j++)
-		Elemento[i][j]=elemento;
+	for (int i = 0; i < FIL; i++)
+		for (int j = 0; j < COL; j++)
+			Elemento[i][j] = elemento;
 
 }
 
@@ -126,28 +126,28 @@ void CMatriz::Inicializar(double elemento)
 //SE INGRESAN LOS ELEMENTOS DE LA MATRIZ, DESDE EL TECLADO, UNO POR UNO
 void CMatriz::IngresarElementos()
 {
- int i,j;
- float elemento;
+	int i, j;
+	float elemento;
 
- for(i=0;i<FIL;i++)
-  for(j=0;j<COL;j++)
-  {
-   printf( "Elemento ( Fil=%i, Col=%i ) = ", i, j );
-   scanf( "%f", &elemento ); // se ingresan los elementos al arreglo Elemento, el cual es miembro de la matriz A
-   Elemento[i][j]= elemento;
-  }
+	for (i = 0; i < FIL; i++)
+		for (j = 0; j < COL; j++)
+		{
+			printf("Elemento ( Fil=%i, Col=%i ) = ", i, j);
+			scanf("%f", &elemento); // se ingresan los elementos al arreglo Elemento, el cual es miembro de la matriz A
+			Elemento[i][j] = elemento;
+		}
 }
 //////////////////////////////////////////////////////////////////////
 //ESCRIBE LA MATRIZ EN LA PANTALLA
 void CMatriz::Escribir()
 {
-   printf("\n\n");
-   for(int i=0;i<FIL;i++)
-   {
-     for(int j=0;j<COL;j++)
-       printf( "%f\t", Elemento[i][j] );
-     printf("\n");
-   }
+	printf("\n\n");
+	for (int i = 0; i < FIL; i++)
+	{
+		for (int j = 0; j < COL; j++)
+			printf("%f\t", Elemento[i][j]);
+		printf("\n");
+	}
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -158,76 +158,76 @@ void CMatriz::Escribir()
 //NEGATIVO DE UNA MATRIZ (-A)
 CMatriz operator-(CMatriz &A)
 {
-   CMatriz *M = new CMatriz;
+	CMatriz *M = new CMatriz;
 
-   *M=A;
+	*M = A;
 
-   for(int i=0;i<A.Fil();i++)
-    for(int j=0;j<A.Col();j++)
-	    M->Elemento[i][j]*=-1;
+	for (int i = 0; i < A.Fil(); i++)
+		for (int j = 0; j < A.Col(); j++)
+			M->Elemento[i][j] *= -1;
 
-    return *M;
+	return *M;
 }
 //////////////////////////////////////////////////////////////////////
 //SUMA DE MATRICES (A+B)
 CMatriz operator+(CMatriz &A, CMatriz &B)
 {
 
-  //Consistencia:
-  if( A.Fil()!=B.Fil() || A.Col()!=B.Col() ){
-	printf("Las matrices no tienen la misma dimensi¢n");
-	return A;
+	//Consistencia:
+	if (A.Fil() != B.Fil() || A.Col() != B.Col()) {
+		printf("Las matrices no tienen la misma dimensi¢n");
+		return A;
 	}
 
-  CMatriz *M = new CMatriz( A.Fil(), A.Col() );
-  for(int i=0;i<A.Fil();i++)
-    for(int j=0;j<A.Col();j++)
-	    M->Elemento[i][j] = A.Elemento[i][j] + B.Elemento[i][j];
-  return *M;
+	CMatriz *M = new CMatriz(A.Fil(), A.Col());
+	for (int i = 0; i < A.Fil(); i++)
+		for (int j = 0; j < A.Col(); j++)
+			M->Elemento[i][j] = A.Elemento[i][j] + B.Elemento[i][j];
+	return *M;
 }
 //////////////////////////////////////////////////////////////////////
 //DIFERENCIA DE MATRICES (A-B)
 CMatriz operator-(CMatriz &A, CMatriz &B)
 {
-  return (A+(-B));
+	return (A + (-B));
 }
 //////////////////////////////////////////////////////////////////////
 //CONSTANTE POR MATRIZ (k*A)
 CMatriz operator*(double k, CMatriz &A)
 {
-  CMatriz *M = new CMatriz(A.Fil(), A.Col());
+	CMatriz *M = new CMatriz(A.Fil(), A.Col());
 
-  for(int i=0;i<A.Fil();i++)
-    for(int j=0;j<A.Col();j++)
-	    M->Elemento[i][j] = k*A.Elemento[i][j];
-  return *M;
+	for (int i = 0; i < A.Fil(); i++)
+		for (int j = 0; j < A.Col(); j++)
+			M->Elemento[i][j] = k * A.Elemento[i][j];
+	return *M;
 }
 //////////////////////////////////////////////////////////////////////
 //MULTIPLICACION DE MATRICES (A*B)
 CMatriz operator*(CMatriz &A, CMatriz &B)
 {
- //Consistencia:  A.Col()==B.Fil()
- if( A.Col() != B.Fil())
- {
-  printf("\nNo se puede multiplicar las matrices");
-  return A;
- }
- else
- {
-  int i,j,k,n=A.Col();
+	//Consistencia:  A.Col()==B.Fil()
+	if (A.Col() != B.Fil())
+	{
+		printf("\nNo se puede multiplicar las matrices");
+		return A;
+	}
+	else
+	{
+		int i, j, k, n = A.Col();
 
-  CMatriz *M = new CMatriz(A.Fil(), B.Col());
-  for(i=0;i<M->Fil();i++)
-    for(j=0;j<M->Col();j++)
-    {
-     double S=0.0;
-     for(k=0;k<n;k++)
-      S+=A.Elemento[i][k]*B.Elemento[k][j];
-     M->Elemento[i][j]=S;
-    }
+		CMatriz *M = new CMatriz(A.Fil(), B.Col());
+		for (i = 0; i < M->Fil(); i++)
+			for (j = 0; j < M->Col(); j++)
+			{
+				double S = 0.0;
+				for (k = 0; k < n; k++)
+					S += A.Elemento[i][k] * B.Elemento[k][j];
+				M->Elemento[i][j] = S;
+			}
 
-  return *M;
- }
+		return *M;
+	}
 
 }
 
@@ -241,32 +241,33 @@ CMatriz operator*(CMatriz &A, CMatriz &B)
 //POR EL METODO DE COFACTORES
 //!!!Nesecita que se le ingrese el orden de la matriz
 //////////////////////////////////////////////////////////////////////
-double DetCofact( CMatriz &A, int orden )
+double DetCofact(CMatriz &A, int orden)
 {
 
- double D=0;
- CMatriz B;
- int x,i,j;
+	double D = 0;
+	CMatriz B;
+	int x, i, j;
 
- if(orden>=2)
- {
+	if (orden >= 2)
+	{
 
-  for(x=0;x<orden;x++)
-  {
+		for (x = 0; x < orden; x++)
+		{
 
-   for(i=0;i<orden-1;i++)
-    for(j=1;j<orden;j++)
-   {
-    if(i<x) B.Elemento[i][j-1] = A.Elemento[i][j];
-    else  B.Elemento[i][j-1] = A.Elemento[i+1][j];
-   }
+			for (i = 0; i < orden - 1; i++)
+				for (j = 1; j < orden; j++)
+				{
+					if (i < x) B.Elemento[i][j - 1] = A.Elemento[i][j];
+					else  B.Elemento[i][j - 1] = A.Elemento[i + 1][j];
+				}
 
-   D += pow(-1,x) * A.Elemento[x][0] * DetCofact( B, orden-1 );
-  }
+			D += pow(-1, x) * A.Elemento[x][0] * DetCofact(B, orden - 1);
+		}
 
-  return D;
+		return D;
 
- }else return A.Elemento[0][0];
+	}
+	else return A.Elemento[0][0];
 
 }
 
@@ -276,13 +277,13 @@ double DetCofact( CMatriz &A, int orden )
 //////////////////////////////////////////////////////////////////////
 double Det(CMatriz &A)
 {
-//Consistencia:
-  if( A.Orden()==-1 ){
-	printf("La matriz no es cuadrada");
-	return 0;
+	//Consistencia:
+	if (A.Orden() == -1) {
+		printf("La matriz no es cuadrada");
+		return 0;
 	}
 
-return DetCofact( A, A.Orden() );
+	return DetCofact(A, A.Orden());
 
 }
 
@@ -300,43 +301,43 @@ return DetCofact( A, A.Orden() );
 CMatriz InvCofact(CMatriz M)
 {
 
-//Consistencia:
-  if( M.Orden()==-1 ){
-	printf("La matriz no es cuadrada");
-	return M;
+	//Consistencia:
+	if (M.Orden() == -1) {
+		printf("La matriz no es cuadrada");
+		return M;
 	}
-//Consistencia: Determinante!=0 para que Exista Inversa
-double DET=Det(M);
-	if(DET==0){
-	printf("La Matriz no es inversible, su determinante es 0");
-	return M;
+	//Consistencia: Determinante!=0 para que Exista Inversa
+	double DET = Det(M);
+	if (DET == 0) {
+		printf("La Matriz no es inversible, su determinante es 0");
+		return M;
 	}
 
-int i,j,x,y;
+	int i, j, x, y;
 
-CMatriz Ct( M.Orden() );
-CMatriz B(M.Orden()-1);
- //se crea la submatriz B de orden n-1
- //pivoteando en x,y
+	CMatriz Ct(M.Orden());
+	CMatriz B(M.Orden() - 1);
+	//se crea la submatriz B de orden n-1
+	//pivoteando en x,y
 
- for(x=0;x<M.Orden();x++)
-  for(y=0;y<M.Orden();y++)
-   {
-    for(i=0;i<M.Orden()-1;i++)
-     for(j=0;j<M.Orden()-1;j++)
-      {
-       if( i<x && j<y  ) B.Elemento[i][j] = M.Elemento[i][j];
-	else if( i<x && j>=y ) B.Elemento[i][j] = M.Elemento[i][j+1];
-	 else if( i>=x && j<y ) B.Elemento[i][j] = M.Elemento[i+1][j];
-	  else if( i>=x && j>=y) B.Elemento[i][j] = M.Elemento[i+1][j+1];
-      }
+	for (x = 0; x < M.Orden(); x++)
+		for (y = 0; y < M.Orden(); y++)
+		{
+			for (i = 0; i < M.Orden() - 1; i++)
+				for (j = 0; j < M.Orden() - 1; j++)
+				{
+					if (i < x && j < y) B.Elemento[i][j] = M.Elemento[i][j];
+					else if (i < x && j >= y) B.Elemento[i][j] = M.Elemento[i][j + 1];
+					else if (i >= x && j < y) B.Elemento[i][j] = M.Elemento[i + 1][j];
+					else if (i >= x && j >= y) B.Elemento[i][j] = M.Elemento[i + 1][j + 1];
+				}
 
-    //Se halla la transpuesta de la matriz de cofactores
-    Ct.Elemento[y][x] = pow(-1,x+y) * Det( B );
+			//Se halla la transpuesta de la matriz de cofactores
+			Ct.Elemento[y][x] = pow(-1, x + y) * Det(B);
 
-   }
+		}
 
-return pow(DET,-1)*Ct;
+	return pow(DET, -1)*Ct;
 }
 
 
@@ -347,74 +348,75 @@ return pow(DET,-1)*Ct;
 CMatriz InvGauss(CMatriz M)
 {
 
- int m=M.Orden();
+	int m = M.Orden();
 
-  //Consistencia:
-  if( m==-1 ){
-	printf("La matriz no es cuadrada");
-	return M;
+	//Consistencia:
+	if (m == -1) {
+		printf("La matriz no es cuadrada");
+		return M;
 	}
 
- double G[_MAX][2*_MAX];//Arreglo Gaussiano
+	double G[_MAX][2 * _MAX];//Arreglo Gaussiano
 
- int i,j,k,s,t;
- double T,tmp,DET=1;
+	int i, j, k, s, t;
+	double T, tmp, DET = 1;
 
- //Se Asigna el contenido de la Matriz M al arreglo Gaussiano G
- for(i=0;i<m;i++)
-	for(j=0;j<m;j++)
-		G[i][j]=M.Elemento[i][j];
+	//Se Asigna el contenido de la Matriz M al arreglo Gaussiano G
+	for (i = 0; i < m; i++)
+		for (j = 0; j < m; j++)
+			G[i][j] = M.Elemento[i][j];
 
- //Se anexa a esta matriz una matriz identidad de orden m para aplicar
- //Reduccion Gaussiana
- for(i=0;i<=m-1;i++)
-	for(j=m;j<=2*m-1;j++){
-		if(i+m==j)G[i][j]=1;else G[i][j]=0;}
-
- //Se reduce la matriz por operaciones elementales comenzando por G[0][0]
- //a una matriz triangular superior
- for(k=0;k<m;k++)
-	for(i=k;i<m;i++){
-			T=G[i][k];
-			for(s=i;s<m-1&&T==0;s++)
-				for(t=0;t<2*m;t++){
-					tmp=G[s][t];
-					G[s][t]=G[s+1][t];
-					G[s+1][t]=tmp;
-				}
-
-			DET*=T;
-			if(T==0)continue;
-
-		for(j=2*m-1;j>=0;j--){	//Se disminuye el valor de j para que el valor de G[i][0] solo sea alterado al final
-			if(i==k)G[i][j]/=T; //se divide a la primera fila por el primer termino de la fila
-			else G[i][j]=G[i][j]/T-G[k][j]; //(A partir de la segunda fila) se divide la fila i por el primer termino de la fila i
-			}
-		  }
-
- //Consistencia
- if(DET==0)
- {
- printf("La Matriz no es inversible, su determinante es 0");
- return M;
- }
-
- //Se reduce la matriz triangular superior de la izquierda de G
- //a una matriz identidad
- for(k=0;k<m;k++)
-	for(t=k+1;t<m;t++){
-		T=G[k][t];
-		for(j=0;j<2*m;j++)
-			G[k][j]-=T*G[t][j];
+	//Se anexa a esta matriz una matriz identidad de orden m para aplicar
+	//Reduccion Gaussiana
+	for (i = 0; i <= m - 1; i++)
+		for (j = m; j <= 2 * m - 1; j++) {
+			if (i + m == j)G[i][j] = 1; else G[i][j] = 0;
 		}
 
- //Por lo tanto en la parte derecha de G queda la matriz Inversa
- //que es asignada de vuelta a M para ser retornada
- for(i=0;i<m;i++)
-	for(j=0;j<m;j++)
-	M.Elemento[i][j]=G[i][j+m];
+	//Se reduce la matriz por operaciones elementales comenzando por G[0][0]
+	//a una matriz triangular superior
+	for (k = 0; k < m; k++)
+		for (i = k; i < m; i++) {
+			T = G[i][k];
+			for (s = i; s < m - 1 && T == 0; s++)
+				for (t = 0; t < 2 * m; t++) {
+					tmp = G[s][t];
+					G[s][t] = G[s + 1][t];
+					G[s + 1][t] = tmp;
+				}
 
- return M;
+			DET *= T;
+			if (T == 0)continue;
+
+			for (j = 2 * m - 1; j >= 0; j--) {	//Se disminuye el valor de j para que el valor de G[i][0] solo sea alterado al final
+				if (i == k)G[i][j] /= T; //se divide a la primera fila por el primer termino de la fila
+				else G[i][j] = G[i][j] / T - G[k][j]; //(A partir de la segunda fila) se divide la fila i por el primer termino de la fila i
+			}
+		}
+
+	//Consistencia
+	if (DET == 0)
+	{
+		printf("La Matriz no es inversible, su determinante es 0");
+		return M;
+	}
+
+	//Se reduce la matriz triangular superior de la izquierda de G
+	//a una matriz identidad
+	for (k = 0; k < m; k++)
+		for (t = k + 1; t < m; t++) {
+			T = G[k][t];
+			for (j = 0; j < 2 * m; j++)
+				G[k][j] -= T * G[t][j];
+		}
+
+	//Por lo tanto en la parte derecha de G queda la matriz Inversa
+	//que es asignada de vuelta a M para ser retornada
+	for (i = 0; i < m; i++)
+		for (j = 0; j < m; j++)
+			M.Elemento[i][j] = G[i][j + m];
+
+	return M;
 
 }
 /*
--- math-and-others/cmatrix/example.cpp
+++ math-and-others/cmatrix/example.cpp
@@ -16,19 +16,19 @@ Pagina Web ( EL MUNDO DEL CAOS ):
 
 #include <conio.h>
 #include "cmatriz.h" //Cabecera con funciones espec¡ficas para
-		     //el ALGEBRA DE MATRICES
+//el ALGEBRA DE MATRICES
 
 void ImprimirTitulo()
 {
- clrscr();
- printf("\t\t\t****CMATRIZ 1.4 DEMO****");
- printf("\n\t\t\tALGEBRA DE MATRICES");
+	clrscr();
+	printf("\t\t\t****CMATRIZ 1.4 DEMO****");
+	printf("\n\t\t\tALGEBRA DE MATRICES");
 }
 
 void pausa()
 {
- printf("\n\nPresione una tecla para continuar...");
- getch();
+	printf("\n\nPresione una tecla para continuar...");
+	getch();
 }
 
 //UN EJEMPLO
@@ -39,189 +39,189 @@ void pausa()
 //a un valor menoral que contiene actualmente
 void main()
 {
- /////////////////////////////////////////////////////////////////////////
- //PRIMER EJEMPLO
- /////////////////////////////////////////////////////////////////////////
- ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
+	/////////////////////////////////////////////////////////////////////////
+	//PRIMER EJEMPLO
+	/////////////////////////////////////////////////////////////////////////
+	ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
 
- printf("\n\nIngresar datos de la matriz A (Matriz cuadrada)");
- printf("\n\nIngrese el orden de la matriz: ");
+	printf("\n\nIngresar datos de la matriz A (Matriz cuadrada)");
+	printf("\n\nIngrese el orden de la matriz: ");
 
- int orden;
- scanf("%i", &orden);
+	int orden;
+	scanf("%i", &orden);
 
- CMatriz A(orden); //se declara una matriz del orden especificado
+	CMatriz A(orden); //se declara una matriz del orden especificado
 
- printf("\nIngrese los elementos de la matriz A:\n\n");
+	printf("\nIngrese los elementos de la matriz A:\n\n");
 
- A.IngresarElementos(); // se ingresan los elementos de la matriz A desde el
-		      // teclado
+	A.IngresarElementos(); // se ingresan los elementos de la matriz A desde el
+				 // teclado
 
- printf("\n\nPresione una tecla para ver la matriz ingresada...");
- getch();
+	printf("\n\nPresione una tecla para ver la matriz ingresada...");
+	getch();
 
- ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
+	ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
 
- printf("\n\nUsted ingres¢ la siguiente Matriz A");
+	printf("\n\nUsted ingres¢ la siguiente Matriz A");
 
- A.Escribir(); //muestra la matriz en pantalla
+	A.Escribir(); //muestra la matriz en pantalla
 
- printf("\n\nPresione una tecla para ver su determinante y su inversa...");
- getch();
+	printf("\n\nPresione una tecla para ver su determinante y su inversa...");
+	getch();
 
- ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
+	ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
 
- printf("\n\nLa determinante de la matriz A es: %f",  Det(A) );
+	printf("\n\nLa determinante de la matriz A es: %f", Det(A));
 
- pausa(); //se hace una pausa
+	pausa(); //se hace una pausa
 
- printf("\n\nLa inversa de la matriz A por el M‚todo de Reducci¢n Gaussiana es:\n");
+	printf("\n\nLa inversa de la matriz A por el M‚todo de Reducci¢n Gaussiana es:\n");
 
- InvGauss(A).Escribir(); //Escribe la inversa de A en pantalla, por el m‚todo de reducci¢n Gaussiana
+	InvGauss(A).Escribir(); //Escribe la inversa de A en pantalla, por el m‚todo de reducci¢n Gaussiana
 
- pausa(); //se hace una pausa
+	pausa(); //se hace una pausa
 
- printf("\n\nLa inversa de la matriz A por el M‚todo de la transpuesta\nde la matriz de cofactores es:\n");
+	printf("\n\nLa inversa de la matriz A por el M‚todo de la transpuesta\nde la matriz de cofactores es:\n");
 
- InvCofact(A).Escribir(); //Escribe la inversa de A en pantalla, por el m‚todo de la transpuesta de la matriz de cofactores
+	InvCofact(A).Escribir(); //Escribe la inversa de A en pantalla, por el m‚todo de la transpuesta de la matriz de cofactores
 
- pausa(); //se hace una pausa
+	pausa(); //se hace una pausa
 
- //LA DIFERENCIA ENTRE InvGauss e InvCofact es que la primera necesita
- //de menos memoria que la segunda, pero la segunda hace menos operaciones
- //que la primera, por la tanto InvCofact es mas r pida
+	//LA DIFERENCIA ENTRE InvGauss e InvCofact es que la primera necesita
+	//de menos memoria que la segunda, pero la segunda hace menos operaciones
+	//que la primera, por la tanto InvCofact es mas r pida
 
- /////////////////////////////////////////////////////////////////////////
- //SEGUNDO EJEMPLO
- /////////////////////////////////////////////////////////////////////////
- ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
+	/////////////////////////////////////////////////////////////////////////
+	//SEGUNDO EJEMPLO
+	/////////////////////////////////////////////////////////////////////////
+	ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
 
- printf("\n\nComprobando las operaciones entre matrices (Suma, Resta, multiplicaci¢n)");
- printf("\n\nA continuaci¢n se le pedir  que ingrese los datos una matriz B, la cual se");
- printf("\noperar  con A. Entonces usted ya sabe que el orden de dicha matriz debe ser");
- printf("\nigual al orden de A, pero si usted quiere probar la facilidad con que CMatriz,");
- printf("\ndetecta errores en las dimensiones entre A y B al hacer operaciones, ingrese");
- printf("\nun orden para B diferente al de A");
+	printf("\n\nComprobando las operaciones entre matrices (Suma, Resta, multiplicaci¢n)");
+	printf("\n\nA continuaci¢n se le pedir  que ingrese los datos una matriz B, la cual se");
+	printf("\noperar  con A. Entonces usted ya sabe que el orden de dicha matriz debe ser");
+	printf("\nigual al orden de A, pero si usted quiere probar la facilidad con que CMatriz,");
+	printf("\ndetecta errores en las dimensiones entre A y B al hacer operaciones, ingrese");
+	printf("\nun orden para B diferente al de A");
 
- printf("\n\nIngresar datos de la matriz B (Matriz cuadrada)");
- printf("\n\nIngrese el orden de la matriz: ");
+	printf("\n\nIngresar datos de la matriz B (Matriz cuadrada)");
+	printf("\n\nIngrese el orden de la matriz: ");
 
- scanf("%i", &orden);
+	scanf("%i", &orden);
 
- CMatriz B(orden); //se declara una matriz del orden especificado
+	CMatriz B(orden); //se declara una matriz del orden especificado
 
- printf("\nIngrese los elementos de la matriz B:\n\n");
+	printf("\nIngrese los elementos de la matriz B:\n\n");
 
- B.IngresarElementos(); // se ingresan los elementos de la matriz A desde el
-		      // teclado
+	B.IngresarElementos(); // se ingresan los elementos de la matriz A desde el
+				 // teclado
 
- printf("\n\nPresione una tecla para ver la matriz ingresada...");
- getch();
+	printf("\n\nPresione una tecla para ver la matriz ingresada...");
+	getch();
 
- ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
+	ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
 
- printf("\n\nUsted ingres¢ la siguiente Matriz B");
+	printf("\n\nUsted ingres¢ la siguiente Matriz B");
 
- B.Escribir(); //muestra la matriz en pantalla
+	B.Escribir(); //muestra la matriz en pantalla
 
- printf("\n\nPresione una tecla para ver las operaciones entre A y B...");
+	printf("\n\nPresione una tecla para ver las operaciones entre A y B...");
 
- ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
+	ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
 
- CMatriz C(orden); //se declara una matriz C para contener la operaci¢n entre A y B
+	CMatriz C(orden); //se declara una matriz C para contener la operaci¢n entre A y B
 
- printf("\nSuma de A y B:");
+	printf("\nSuma de A y B:");
 
- C = A + B; //Suma de matrices
+	C = A + B; //Suma de matrices
 
- C.Escribir(); //se escribe la matriz C en pantalla
+	C.Escribir(); //se escribe la matriz C en pantalla
 
- pausa(); //se hace una pausa
+	pausa(); //se hace una pausa
 
- printf("\nDiferencia de A y B:");
+	printf("\nDiferencia de A y B:");
 
- C = A - B; //Suma de matrices
+	C = A - B; //Suma de matrices
 
- C.Escribir(); //se escribe la matriz C en pantalla
+	C.Escribir(); //se escribe la matriz C en pantalla
 
- pausa(); //se hace una pausa
+	pausa(); //se hace una pausa
 
- printf("\nMultiplicaci¢n de A y B:");
+	printf("\nMultiplicaci¢n de A y B:");
 
- C = A * B; //Suma de matrices
+	C = A * B; //Suma de matrices
 
- C.Escribir(); //se escribe la matriz C en pantalla
+	C.Escribir(); //se escribe la matriz C en pantalla
 
- pausa(); //se hace una pausa
+	pausa(); //se hace una pausa
 
- /////////////////////////////////////////////////////////////////////////
- //TERCER EJEMPLO
- /////////////////////////////////////////////////////////////////////////
- ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
+	/////////////////////////////////////////////////////////////////////////
+	//TERCER EJEMPLO
+	/////////////////////////////////////////////////////////////////////////
+	ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
 
- printf("\n\nComo £ltimo ejemplo vamos a resolver un sistema de 3 ecuaciones");
- printf("\ncon 3 inc¢gnitas:");
+	printf("\n\nComo £ltimo ejemplo vamos a resolver un sistema de 3 ecuaciones");
+	printf("\ncon 3 inc¢gnitas:");
 
- //matriz de coeficientes
- A=CMatriz(3);//se declara una matriz de orden 3
+	//matriz de coeficientes
+	A = CMatriz(3);//se declara una matriz de orden 3
 
- //matriz de terminos independientes
- B=CMatriz(3,1);//se declara una matriz con 3 filas y una columna
+	//matriz de terminos independientes
+	B = CMatriz(3, 1);//se declara una matriz con 3 filas y una columna
 
- //matriz de soluciones
- CMatriz X(3,1);//se declara una matriz con 3 filas y una columna
+	//matriz de soluciones
+	CMatriz X(3, 1);//se declara una matriz con 3 filas y una columna
 
- //Se crea el sistema matricial A*X = B
- //se obtienen los elementos de la matriz A y la matriz B
- int i,j;
- float elemento;
+	//Se crea el sistema matricial A*X = B
+	//se obtienen los elementos de la matriz A y la matriz B
+	int i, j;
+	float elemento;
 
- printf("\n\nIngrese los coeficientes de las 3 ecuaciones:");
+	printf("\n\nIngrese los coeficientes de las 3 ecuaciones:");
 
- for(i=0;i<3;i++)
- {
-  printf("\n\nIngrese los coeficientes a%i, b%i, c%i y d%i", i+1,i+1,i+1,i+1 );
-  printf("\nde la ecuaci¢n a%ix + b%iy + c%iz = d%i\n\n",
-	 i+1,i+1,i+1,i+1 );
-  for(j=0;j<4;j++)
-  {
-   printf("%c%i = ",'a'+j, i+1);
-   scanf( "%f", &elemento);
-   if(j<3)
-    A.Elemento[i][j]=elemento;
-   else
-    B.Elemento[i][0]=elemento;
-  }
- }
-  //por el Algebra matricial sabemos que la soluci¢n de  A*X = B
-  //es X = Inversa(A)*B
+	for (i = 0; i < 3; i++)
+	{
+		printf("\n\nIngrese los coeficientes a%i, b%i, c%i y d%i", i + 1, i + 1, i + 1, i + 1);
+		printf("\nde la ecuaci¢n a%ix + b%iy + c%iz = d%i\n\n",
+			i + 1, i + 1, i + 1, i + 1);
+		for (j = 0; j < 4; j++)
+		{
+			printf("%c%i = ", 'a' + j, i + 1);
+			scanf("%f", &elemento);
+			if (j < 3)
+				A.Elemento[i][j] = elemento;
+			else
+				B.Elemento[i][0] = elemento;
+		}
+	}
+	//por el Algebra matricial sabemos que la soluci¢n de  A*X = B
+	//es X = Inversa(A)*B
 
-  //asignamos a X la soluci¢n del sistema
-  X = InvGauss(A)*B;
+	//asignamos a X la soluci¢n del sistema
+	X = InvGauss(A)*B;
 
-  printf("\nLa soluci¢n del sistema es:");
+	printf("\nLa soluci¢n del sistema es:");
 
-  //mostramos la soluci¢n del sistema
-  for(i=0;i<3;i++)
-  printf("\nX%i = %f", i+1, X.Elemento[i][0]);
+	//mostramos la soluci¢n del sistema
+	for (i = 0; i < 3; i++)
+		printf("\nX%i = %f", i + 1, X.Elemento[i][0]);
 
- pausa(); //se hace una pausa
+	pausa(); //se hace una pausa
 
- ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
+	ImprimirTitulo(); //T¡tulo de la Demo de CMatriz 1.4
 
- gotoxy(1,4);
- printf("\nfue programado por:");
- printf("\n JOSE LUIS DE LA CRUZ LAZARO");
- printf("\ncorreos:");
- printf("\n jcruz@ec-red.com");
- printf("\n ramondc@hotmail.com");
- printf("\nPagina Web ( EL MUNDO DEL CAOS ):");
- printf("\n http://www.geocities.com/joseluisdl");
+	gotoxy(1, 4);
+	printf("\nfue programado por:");
+	printf("\n JOSE LUIS DE LA CRUZ LAZARO");
+	printf("\ncorreos:");
+	printf("\n jcruz@ec-red.com");
+	printf("\n ramondc@hotmail.com");
+	printf("\nPagina Web ( EL MUNDO DEL CAOS ):");
+	printf("\n http://www.geocities.com/joseluisdl");
 
 
- gotoxy(30,15);
- printf("FIN DE LA DEMO... ;(");
+	gotoxy(30, 15);
+	printf("FIN DE LA DEMO... ;(");
 
- getch();
+	getch();
 
- }
+}
