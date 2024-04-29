2535961b3225e9e24e9c0eaa0c5e270e63311157
yacshagames <ramondc@hotmail.com>
2024-04-28 21:54:53 -0500

    Update math-and-others\cmatrix - AddOns Updates with many improves
    - AddOns (version2) is improved: pause, pauseAndExit and pauseYesOrNot functions added

--- math-and-others/cmatrix/AddOns.cpp
+++ math-and-others/cmatrix/AddOns.cpp
@@ -15,8 +15,31 @@ void AddOns::chronoEnd() {
 	const auto time_end{ std::chrono::steady_clock::now() };
 	const std::chrono::duration<double> elapsed_seconds{ time_end - time_start };
 	std::cout << "\nElapsed time: " << elapsed_seconds.count() << " seconds" << std::endl;
-
-	textcolor(LIGHTGRAY);
-	std::cout << "Press any key to show solutions..." << std::endl;
-	cgetch();
 }
+
+char AddOns::pause()
+{
+	textcolor(LIGHTGRAY);
+	std::cout << "Press any key to continue..." << std::endl;
+	return cgetch();
+}
+
+bool AddOns::pauseAndExit()
+{
+	textcolor(LIGHTGRAY);
+	std::cout << "Press any key to continue or SPACE key to return to menu..." << std::endl;
+	return cgetch();
+
+	if (cgetch() == 32) // SPACE key = 32
+		return true;
+
+	return false;
+}
+
+bool AddOns::pauseYesOrNot(std::string message)
+{
+	textcolor(WHITE);
+	std::cout << message;
+	return toupper(cgetch()) == 'Y';
+}
+
--- math-and-others/cmatrix/AddOns.h
+++ math-and-others/cmatrix/AddOns.h
@@ -1,12 +1,54 @@
 #pragma once
+/*********************************************************************
+:: ADDONS ::
+
+Common functions used in console mode programs
+
+Developed by:
+
+	JOSE LUIS DE LA CRUZ LAZARO
+	ramondc@hotmail.com
+
+	UNIVERSIDAD NACIONAL DE INGENIERIA
+	Faculty of Electrical and Electronic Engineering
+	Lima-Peru
+
+	YACSHA - Software & Desing
+	>> The World of chaos - EL MUNDO DEL CAOS - Unlimited Programming
+
+HISTORY...
+
+  >> Version 2 - 28-IV-2024
+	- pause, pauseAndExit and pauseYesOrNot functions added	
+
+  >> Version 1 - 04-IV-2024
+	- chronoStart and chronoEnd functions added	
+
+**********************************************************************/
+
 #include <chrono> // chronometers functions
+#include <string>
 
 class AddOns
 {
 public:
 	// chronometers functions
+	
+	// Start the stopwatch
 	static void chronoStart();
+
+	// Ends the stopwatch and shows the elapsed time
 	static void chronoEnd();
+
+	// Pauses until a key is pressed
+	// Display a message to notify the user
+	static char pause();
+
+	// returns true if the user presses the space key, and false if the user presses any other key
+	static bool pauseAndExit();
+
+	// returns true if Y key is pressed or false if another key is pressed
+	static bool pauseYesOrNot(std::string message);
 
 	// chronometers variables
 	static std::chrono::time_point<std::chrono::steady_clock> time_start;
