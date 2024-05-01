643dfb7b03df5fd339729c699e3f197a0fcaf964
yacshagames <ramondc@hotmail.com>
2024-05-01 13:24:47 -0500

    Update visual-c++\BezierCurve - Original version for VC++ 6.0

---
+++ visual-c++/BezierCurve/resource.h
@@ -0,0 +1,48 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Clase Transformaciones.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CLASETRANSFORMACIONES_DIALOG 102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
+#define IDC_BUTTON3                     1002
+#define IDC_BUTTON4                     1003
+#define IDC_BUTTON5                     1004
+#define IDC_BUTTON6                     1005
+#define IDC_BUTTON7                     1006
+#define IDC_BUTTON8                     1007
+#define IDC_BUTTON9                     1008
+#define IDC_BUTTON10                    1009
+#define IDC_BUTTON11                    1010
+#define IDC_BUTTON12                    1011
+#define IDC_BUTTON13                    1012
+#define IDC_BUTTON14                    1013
+#define IDC_Dibujo                      1014
+#define IDC_BUTTON16                    1015
+#define IDC_Valinit                     1016
+#define IDC_Proyeccion                  1017
+#define IDC_Cex                         1018
+#define IDC_Rox                         1019
+#define IDC_DENSIDAD                    1020
+#define IDC_Nox                         1021
+#define IDC_Cey                         1022
+#define IDC_Roy                         1023
+#define IDC_Noy                         1024
+#define IDC_Cez                         1025
+#define IDC_Roz                         1026
+#define IDC_Noz                         1027
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1021
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
