cd7ee81f92ecff3c87b8966e1907f74f9899336a
yacshagames <ramondc@hotmail.com>
2024-05-04 11:16:02 -0500

    Update visual-c++\LagrangeCurve - Original version for VC++ 6.0

---
+++ visual-c++/LagrangeCurve/Clase Transformaciones.clw
@@ -0,0 +1,121 @@
+; CLW file contains information for the MFC ClassWizard
+
+[General Info]
+Version=1
+LastClass=CClaseTransformacionesDlg
+LastTemplate=CDialog
+NewFileInclude1=#include "stdafx.h"
+NewFileInclude2=#include "Clase Transformaciones.h"
+
+ClassCount=3
+Class1=CClaseTransformacionesApp
+Class2=CClaseTransformacionesDlg
+Class3=CAboutDlg
+
+ResourceCount=5
+Resource1=IDD_ABOUTBOX
+Resource2=IDR_MAINFRAME
+Resource3=IDD_CLASETRANSFORMACIONES_DIALOG
+Resource4=IDD_ABOUTBOX (Spanish (Modern))
+Resource5=IDD_CLASETRANSFORMACIONES_DIALOG (Spanish (Modern))
+
+[CLS:CClaseTransformacionesApp]
+Type=0
+HeaderFile=Clase Transformaciones.h
+ImplementationFile=Clase Transformaciones.cpp
+Filter=N
+
+[CLS:CClaseTransformacionesDlg]
+Type=0
+HeaderFile=Clase TransformacionesDlg.h
+ImplementationFile=Clase TransformacionesDlg.cpp
+Filter=D
+BaseClass=CDialog
+VirtualFilter=dWC
+LastObject=IDC_DENSIDAD
+
+[CLS:CAboutDlg]
+Type=0
+HeaderFile=Clase TransformacionesDlg.h
+ImplementationFile=Clase TransformacionesDlg.cpp
+Filter=D
+
+[DLG:IDD_ABOUTBOX]
+Type=1
+ControlCount=4
+Control1=IDC_STATIC,static,1342177283
+Control2=IDC_STATIC,static,1342308352
+Control3=IDC_STATIC,static,1342308352
+Control4=IDOK,button,1342373889
+Class=CAboutDlg
+
+
+[DLG:IDD_CLASETRANSFORMACIONES_DIALOG]
+Type=1
+ControlCount=3
+Control1=IDOK,button,1342242817
+Control2=IDCANCEL,button,1342242816
+Control3=IDC_STATIC,static,1342308352
+Class=CClaseTransformacionesDlg
+
+[DLG:IDD_CLASETRANSFORMACIONES_DIALOG (Spanish (Modern))]
+Type=1
+Class=CClaseTransformacionesDlg
+ControlCount=47
+Control1=IDOK,button,1342242817
+Control2=IDC_BUTTON1,button,1342242816
+Control3=IDC_STATIC,static,1342312448
+Control4=IDC_BUTTON2,button,1342242816
+Control5=IDC_BUTTON3,button,1342242816
+Control6=IDC_BUTTON4,button,1342242816
+Control7=IDC_BUTTON5,button,1342242816
+Control8=IDC_BUTTON6,button,1342242816
+Control9=IDC_BUTTON7,button,1342242817
+Control10=IDC_BUTTON8,button,1342242816
+Control11=IDC_STATIC,static,1342312448
+Control12=IDC_BUTTON9,button,1342242816
+Control13=IDC_BUTTON10,button,1342242816
+Control14=IDC_BUTTON11,button,1342242816
+Control15=IDC_BUTTON12,button,1342242816
+Control16=IDC_BUTTON13,button,1342242816
+Control17=IDC_BUTTON14,button,1342242816
+Control18=IDC_STATIC,static,1342312448
+Control19=IDC_BUTTON16,button,1342242816
+Control20=IDC_Dibujo,button,1342242817
+Control21=IDC_Valinit,button,1342242817
+Control22=IDC_STATIC,static,1342312448
+Control23=IDC_Proyeccion,button,1342242817
+Control24=IDC_STATIC,static,1342312448
+Control25=IDC_STATIC,static,1342308352
+Control26=IDC_STATIC,static,1342308352
+Control27=IDC_STATIC,static,1342308352
+Control28=IDC_STATIC,static,1342308352
+Control29=IDC_STATIC,static,1342308352
+Control30=IDC_STATIC,static,1342308352
+Control31=IDC_STATIC,static,1342308352
+Control32=IDC_STATIC,static,1342308352
+Control33=IDC_STATIC,static,1342308352
+Control34=IDC_STATIC,static,1342308352
+Control35=IDC_STATIC,static,1342308352
+Control36=IDC_STATIC,static,1342308352
+Control37=IDC_Cex,edit,1350639744
+Control38=IDC_Rox,edit,1350639744
+Control39=IDC_Nox,edit,1350639744
+Control40=IDC_Cey,edit,1350639744
+Control41=IDC_Roy,edit,1350639744
+Control42=IDC_Noy,edit,1350639744
+Control43=IDC_Cez,edit,1350639744
+Control44=IDC_Roz,edit,1350639744
+Control45=IDC_Noz,edit,1350639744
+Control46=IDC_DENSIDAD,edit,1350631552
+Control47=IDC_STATIC,static,1342308352
+
+[DLG:IDD_ABOUTBOX (Spanish (Modern))]
+Type=1
+Class=?
+ControlCount=4
+Control1=IDC_STATIC,static,1342177283
+Control2=IDC_STATIC,static,1342308480
+Control3=IDC_STATIC,static,1342308352
+Control4=IDOK,button,1342373889
+
---
+++ visual-c++/LagrangeCurve/Clase TransformacionesDlg.h
@@ -0,0 +1,133 @@
+// Clase TransformacionesDlg.h : header file
+//
+
+#if !defined(AFX_CLASETRANSFORMACIONESDLG_H__D94C0027_088B_11D4_B69F_0000214974A3__INCLUDED_)
+#define AFX_CLASETRANSFORMACIONESDLG_H__D94C0027_088B_11D4_B69F_0000214974A3__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+/////////////////////////////////////////////////////////////////////////////
+// CClaseTransformacionesDlg dialog
+
+class CClaseTransformacionesDlg : public CDialog
+{
+// Construction
+public:
+	CClaseTransformacionesDlg(CWnd* pParent = NULL);	// standard constructor
+// Variables, Lista de Arreglos y Procedimientos
+
+
+	double mt[5][5];                //Matriz de Trnasformacion
+	double mm[5][5];                //Matriz de datos de transformacion
+    
+	double p[8][5];                 //Valores de los puntos en 3d
+	double p3d[8][5];               //Puntos transformados en 3d
+	double p2d[8][5];               //Puntos transformados en 2d
+	double PtoAux[8];               //Vector Punto Auxiliar
+	double pm[8][5];                //Puntos - Monitor 2d
+	double PtoAuxM[8];              //Vector Puntos Monitor 2d
+
+	double Q[8][8][5];				//Arreglo Qs
+	double A[8][5];					//Vector As
+	double T[8];					//Vector Ts
+	double tt;						//t
+	int densidad;					//densidad
+    
+	double e3d[4][5];               //Valores de los ejes en 3d
+	double em[4][4];                //Puntos - Monitor 2d
+
+	double pi;                      //Valor de Pi
+
+    double anga;                    //Angulo alpha
+    double angb;                    //Angulo beta
+    double Cx;                      //Centro X
+    double Cy;                      //Centro Y
+	int n;                          //Cantidad Puntos - 1
+
+	double dividendo;               //Dividendo m de Proyecciones
+	double divisor;                 //Divisor n de Proyecciones
+	double centro[4];               //Centro de Proyeccion
+	double ro[4];                   //Punto del Plano Proyeccion
+	double normal[4];               //Normal del Plano Proyeccion
+
+	double mataux[5][5];
+	double b[5];
+	double c[5];
+
+	void Transformar_puntos3d();                               //Transforma los puntos a 2D
+	void Transformar_eje3d();                                  //Transforma el eje a 2D
+    double rad(double angulo);                                 //Conversion en radianes
+    void MatxMat();                                            //Matriz x Matriz
+	void MatxPto();                                            //Matriz x Punto
+	double W(int i, double tt);
+	void Obtener_An();										   //Obtener An
+	void Trasladar(double Tx, double Ty, double Tz);           //Trasladar
+	void Escalar(double Ex, double Ey, double Ez);             //Escalar
+	void Rotar(int Eje, double ang);                           //Rotar
+	void Proyectar();                                          //Proyectar
+	void Dibujar_Cuadrado();                                   //Dibujar Cuadrado
+	void DibujaEje();
+
+    
+
+// Dialog Data
+	//{{AFX_DATA(CClaseTransformacionesDlg)
+	enum { IDD = IDD_CLASETRANSFORMACIONES_DIALOG };
+	double	m_Cex;
+	double	m_Cey;
+	double	m_Cez;
+	double	m_Nox;
+	double	m_Noy;
+	double	m_Noz;
+	double	m_Rox;
+	double	m_Roy;
+	double	m_Roz;
+	int		m_densidad;
+	//}}AFX_DATA
+
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CClaseTransformacionesDlg)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+	//}}AFX_VIRTUAL
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	//{{AFX_MSG(CClaseTransformacionesDlg)
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	afx_msg void OnAceptar();
+	afx_msg void OnTrasXPos();
+	afx_msg void OnTrasxN();
+	afx_msg void OnTrasYPos();
+	afx_msg void OnTrasyN();
+	afx_msg void OnTrasZPos();
+	afx_msg void OnTraszN();
+	afx_msg void OnEje3D();
+	afx_msg void OnRotXP();
+	afx_msg void OnRotXN();
+	afx_msg void OnRotYP();
+	afx_msg void OnRotYN();
+	afx_msg void OnRotZP();
+	afx_msg void OnRotZN();
+	afx_msg void OnEscP();
+	afx_msg void OnEscN();
+	afx_msg void OnDibujo();
+	afx_msg void OnValinit();
+	afx_msg void OnProyeccion();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_CLASETRANSFORMACIONESDLG_H__D94C0027_088B_11D4_B69F_0000214974A3__INCLUDED_)
