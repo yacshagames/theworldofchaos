1d65ec6c76861f995ce6d7ce8732648b30ad2f13
yacshagames <ramondc@hotmail.com>
2024-05-05 00:46:57 -0500

    Update visual-c++\3DRotatingSquare - Original version for VC++ 6.0

---
+++ visual-c++/3DRotatingSquare/resource.h
@@ -0,0 +1,41 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Cuadrado3D.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CUADRADO3D_DIALOG           102
+#define IDR_MAINFRAME                   128
+#define IDC_ROTARX                      1002
+#define IDC_ROTARY                      1003
+#define IDC_ROTARZ                      1004
+#define IDC_TRASLADARX                  1005
+#define IDC_TRASLADARY                  1006
+#define IDC_TRASLADARZ                  1007
+#define IDC_REFLEJARXY                  1008
+#define IDC_REFLEJARXZ                  1009
+#define IDC_REFLEJARYZ                  1010
+#define IDC_REINICIAR                   1011
+#define IDC_SENTIDO                     1012
+#define IDC_DIRECCION                   1013
+#define IDC_LIMPIARREFLEXION            1015
+#define IDC_RADIO2                      1017
+#define IDC_REFLEJARORIGEN              1018
+#define IDC_REFLEJARX                   1019
+#define IDC_REFLEJARY                   1020
+#define IDC_REFLEJARZ                   1021
+#define IDC_MAS                         1024
+#define IDC_MENOS                       1025
+#define IDC_FONDO                       1026
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1027
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
